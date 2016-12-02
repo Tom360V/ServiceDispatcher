7e8f0810748d591939d14dfe8daa3622f0fd8112
Tom360v <mail2tommy@gmail.com>
2016-12-02 09:25:14 +0100

    add Service Dispatcher code

---
+++ src/ServiceDispatcherQueue.c
@@ -0,0 +1,154 @@
+/*
+ *
+ *  Created on: Nov 4, 2016
+ *      Author: nxa03718
+ */
+#include <string.h>
+
+#include "ServiceDispatcherQueue.h"
+#include "ServiceDispatcherRouter.h"
+#include "fifo.h"
+
+/******************************************************************************
+ * PRIVATE VARIABLE
+ *****************************************************************************/
+#define SDQ_ID_SEND         (0)
+#define SDQ_ID_RECV         (1)
+#define SDQ_FIFO_SIZE       (10)
+
+#define MIN(a,b)            ((a<b)?a:b)
+
+typedef struct sdqItem_t
+{
+    fpDelivered_t cbDeliverd;
+    uint8_t topic[SDQ_Q_TOPIC];
+    uint8_t topicLength;
+    uint8_t data [SDQ_Q_DATA];
+    uint8_t dataLength;
+    uint8_t handle;
+}sdqItem_t;
+
+static sdqItem_t sdqFIFO_RECV[SDQ_FIFO_SIZE];
+static sdqItem_t sdqFIFO_SEND[SDQ_FIFO_SIZE];
+
+fifo_t fifo_send, fifo_recv;
+
+static fpSDQ_SendTo_t   sdqfpToService;
+static fpSDQ_SendTo_t   sdqfpToCommunication;
+
+/******************************************************************************
+ * PRIVATE FUNCTIONS
+ *****************************************************************************/
+
+/******************************************************************************
+ * sdq_AddToQueue
+ * Add new services to our service list
+ * param pActionList [in]   pointer to array with actions
+ * param nofActions  [in]   number of actions in the actionlist
+ * param topic       [in]   topic id
+ * param topicName   [in]   topic name in human-readable-text
+ * return:                  nothing
+ */
+static SDHandle_t sdq_AddToQueue(fifo_t *pfifo, uint8_t *topic, uint8_t topicLength, uint8_t *data, uint8_t dataLength, fpDelivered_t cbDelivered)
+{
+    SDHandle_t sdqHandle = -1;
+    sdqItem_t item;
+
+    if(0 < FIFO_FreeSpace(pfifo))
+    {
+        item.cbDeliverd = *cbDelivered;
+
+        item.topicLength = MIN(topicLength, SDQ_Q_TOPIC);
+        memset(item.topic, 0x0,   item.topicLength);
+        memcpy(item.topic, topic, item.topicLength);
+
+        item.dataLength  = MIN(dataLength,  SDQ_Q_DATA);
+        memset(item.data, 0x0,  item.dataLength);
+        memcpy(item.data, data, item.dataLength);
+
+        sdqHandle = FIFO_AddItem(pfifo, (void*)&item);
+    }
+
+    return sdqHandle;
+}
+
+void sdq_CheckForActions(fifo_t *pfifo, fpSDQ_SendTo_t fpSendTo)
+{
+    sdqItem_t item;
+    if(NULL != fpSendTo)
+    {
+        if(0 <= FIFO_GetItem(pfifo, (void*)&item))
+        {
+            if (0 == fpSendTo(item.handle, item.topic, item.topicLength, item.data, item.dataLength))
+            {
+                if (NULL != item.cbDeliverd)
+                {
+                    (item.cbDeliverd)(item.handle, 1);
+                }
+            }
+            else
+            {
+                //did not succeed to handle this item, so put it back in the fifo
+                FIFO_AddItem(pfifo, (void*)&item);
+            }
+        }
+    }
+}
+
+static void sdq_Initialize(fpSDQ_SendTo_t fpToService, fpSDQ_SendTo_t fpToCommunication)
+{
+    int8_t itemSize = sizeof(sdqItem_t);
+
+    FIFO_Init(&fifo_send, &sdqFIFO_SEND, itemSize, sizeof(sdqFIFO_SEND)/itemSize);
+    FIFO_Init(&fifo_recv, &sdqFIFO_RECV, itemSize, sizeof(sdqFIFO_RECV)/itemSize);
+
+    sdqfpToService       = fpToService;
+    sdqfpToCommunication = fpToCommunication;
+}
+
+
+/******************************************************************************
+ * PUBLIC FUNCTIONS
+ *****************************************************************************/
+void SDQ_Init(fpSDQ_SendTo_t fpToCommunication)
+{
+    sdq_Initialize(&SDR_Rout, fpToCommunication);
+}
+
+void SDQ_Init_ExternalRouter(fpSDQ_SendTo_t fpToService, fpSDQ_SendTo_t fpToCommunication)
+{
+    sdq_Initialize(fpToService, fpToCommunication);
+}
+
+void SDQ_Thread()
+{
+    sdq_CheckForActions(&fifo_send, sdqfpToCommunication);
+    sdq_CheckForActions(&fifo_recv, sdqfpToService);
+}
+
+SDHandle_t SDQ_AddToRecvQueue(uint8_t *topic, uint8_t topicLength, uint8_t *data, uint8_t dataLength, fpDelivered_t cbDelivered)
+{
+    return sdq_AddToQueue(&fifo_recv, topic, topicLength, data, dataLength, cbDelivered);
+}
+
+SDHandle_t SDQ_AddToSendQueue(eTOPIC_t topic, uint8_t action, uint8_t *data, uint8_t dataLength, fpDelivered_t cbDelivered)
+{
+    uint8_t t[3];
+    t[0] = (char)topic;
+    t[1] = '/';
+    t[2] = action;
+
+    return sdq_AddToQueue(&fifo_send, t, 3, data, dataLength, cbDelivered);
+}
+
+/*
+int8_t SDQ_DeleteFromRecvQueue(SDHandle_t sdqHandle)
+{
+    return sdq_DeleteFromQueue(SDQ_ID_RECV, sdqHandle);
+}
+
+int8_t SDQ_DeleteFromSendQueue(SDHandle_t sdqHandle)
+{
+    return sdq_DeleteFromQueue(SDQ_ID_RECV, sdqHandle);
+}
+*/
---
+++ src/ServiceDispatcherRouter.c
@@ -0,0 +1,188 @@
+/*
+ * ServiceDispatcherRouter.c
+ *
+ *  Created on: Nov 4, 2016
+ *      Author: nxa03718
+ */
+#include <string.h>
+
+#include "ServiceDispatcher.h"
+#include "ServiceDispatcherRouter.h"
+/******************************************************************************
+ * PRIVATE DATATYPES
+ ******************************************************************************/
+typedef struct
+{
+    RemoteFunctionItem_t *actions;
+    eTOPIC_t topic;
+    char    *topicName;
+    uint8_t nofActions;
+}actionItem_t;
+
+typedef struct
+{
+    actionItem_t services[SD_NOF_TOPICS];
+    int8_t nofServices;
+}ServicesList_t;
+
+/******************************************************************************
+ * LOCAL VARIABLE
+ ******************************************************************************/
+static ServicesList_t srvs;
+
+/******************************************************************************
+ * PRIVATE FUNCTIONS
+ ******************************************************************************/
+
+/******************************************************************************
+ * Search in srvs if there is a topicId match
+ * return value: >= 0 : return value is a valid serviceId
+ *                < 0 : no result found
+ */
+static int8_t sdr_SearchServiceByTopicId(eTOPIC_t topic)
+{
+    int8_t idx = srvs.nofServices-1;
+    while(idx >= 0)
+    {
+        if (srvs.services[idx].topic == topic)
+        {
+            break;
+        }
+        idx--;
+    }
+    return idx;
+}
+
+/******************************************************************************
+ * Search in srvs if there is a topicName match
+ * return value: >= 0 : return value is a valid serviceId
+ *                < 0 : no result found
+ */
+static int8_t sdr_SearchServiceByTopicName(char *topicName)
+{
+    int8_t idx = srvs.nofServices -1;
+    while(idx >=0)
+    {
+        if(0 == strcmp(srvs.services[idx].topicName, topicName))
+        {
+            break;
+        }
+        idx--;
+    }
+    return idx;
+}
+
+/******************************************************************************
+ * Parse topic(string) into topic part and action part
+ * param topic:  [in/out]   expect that incomming string is like "xxxxx/y"
+ *                          multiple x, single y!
+ *                          return value is only the x part
+ * param action: [out]      return the y part of topic
+ * return value: >= 0 : return value is a valid serviceId
+ *                < 0 : no result found
+ */
+int8_t sdr_ParseTopic(char *topic, uint8_t *action)
+{
+    action = (uint8_t*) strchr(topic, (int)'/');
+    if(NULL != action)
+    {
+        *action = 0;
+        action += 1;
+    }
+
+    return (action!=NULL);
+}
+
+/******************************************************************************
+ * PUBLIC FUNCTIONS
+ *****************************************************************************/
+
+/******************************************************************************
+ * SDR_Init, nothing to init at the moment...
+ */
+void   SDR_Init()
+{
+
+}
+
+/******************************************************************************
+ * SDR_SubscribeService
+ * Add new services to our service list
+ * param pActionList [in]   pointer to array with actions
+ * param nofActions  [in]   number of actions in the actionlist
+ * param topic       [in]   topic id
+ * param topicName   [in]   topic name in human-readable-text
+ * return:                  nothing
+ */
+void SDR_SubscribeService(RemoteFunctionItem_t *pActionList, uint8_t nofActions, eTOPIC_t topic, char *topicName)
+{
+    if(srvs.nofServices < SD_NOF_TOPICS)
+    {
+//        LOG(myName, eLOG_Info, "Add List: %s", *topic);
+        srvs.services[srvs.nofServices].actions     = pActionList;
+        srvs.services[srvs.nofServices].topic       = topic;
+        srvs.services[srvs.nofServices].topicName   = topicName;
+        srvs.services[srvs.nofServices].nofActions  = nofActions;
+        srvs.nofServices++;
+    }
+    else
+    {
+//        LOG(myName, eLOG_Error, "Message list full");
+    }
+}
+
+/******************************************************************************
+ * SDR_Rout
+ * Search the service list for a service with the same id/name and a matching action
+ * param handle      [in]   NOT USED?!?!?
+ * param topic       [in]   string pointer, contains id and action: "xxxx/y"
+ *                              multiple x possible, only one y
+ *                              when one x, x is a topicId, else x is topicName
+ * param topicLength [in]   length of topic
+ * param data        [in]   can contain data for the function to call
+ * param dataLength  [in]   size of data
+ * return: >= 0 when succeed
+ *         <  0 when not succeed
+ */
+int8_t SDR_Rout(SDHandle_t handle, uint8_t *topic, uint8_t topicLength, uint8_t *data, uint8_t dataLength)
+{
+    int8_t idxService = -1;
+    RemoteFunctionItem_t *pActionList;
+
+    uint8_t action;
+    if( (topicLength==3) &&
+        (topic[1]=='/') )
+    {
+        action = topic[2];
+        idxService = sdr_SearchServiceByTopicId((eTOPIC_t)topic[0]);
+    }
+    else if(topicLength >3)
+    {
+        if(0 == sdr_ParseTopic((char*)topic, &action))
+        {
+            idxService = sdr_SearchServiceByTopicName((char*)topic);
+        }
+    }
+
+    if(0 > idxService)
+    {
+        return -1;
+    }
+
+    if(srvs.services[idxService].nofActions <= action)
+    {
+        return -2;
+    }
+
+    //Matching list found, now search for command!
+    pActionList = srvs.services[idxService].actions;
+    pActionList += action;
+    if( (pActionList->remoteFunctionId == action) && //check if id matches requested action, just to be sure!
+        (pActionList->remoteFunction != NULL) )
+    {
+//            LOG(myName, eLOG_Info, "Execute FP:%d, %d", idxList,idxMsg);
+        (*pActionList->remoteFunction)(data);
+    }
+
+    return 0;
+}
